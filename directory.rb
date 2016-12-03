b26c906d3463e272aa4196162fef9639f670751a
James Turner <mail.jt@gmail.com>
2016-12-03 20:27:44 +0000

    made student list interactive

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 #array of students
+=begin
 students =[
   {name: "Dr. Hannibal Lecter",         cohort: :november},
   {name: "Darth Vader",                 cohort: :november},
@@ -11,6 +12,19 @@ students =[
   {name: "The Jocker",                  cohort: :november},
   {name: "Joffrey Baratheon",           cohort: :november},
   {name: "Norman  Bates",               cohort: :november}]
+=end
+def input_students
+  puts "please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -27,6 +41,7 @@ def print_footer names
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print students
 print_footer students
