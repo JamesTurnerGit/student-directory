bc90a1bac0e2a47026b831b9c05c08d99f44ee9b
James Turner <mail.jt@gmail.com>
2016-12-03 20:13:44 +0000

    broke program structure up into methods

--- directory.rb
+++ directory.rb
@@ -1,5 +1,4 @@
-student_count = 11
-#array students
+#array of students
 students =["Dr. Hannibal Lecter",
            "Darth Vader",
            "Nurse Ratched",
@@ -12,10 +11,21 @@ students =["Dr. Hannibal Lecter",
            "Joffrey Baratheon",
            "Norman  Bates"]
 
-puts "The students of Villains Academy"
-puts "---------------"
-students.each do |student|
-  puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "---------------"
 end
-#then print total number
-puts "Overall, we have #{students.count} great students"
+
+def print names
+  names.each do |student|
+    puts student
+  end
+end
+
+def print_footer names
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print students
+print_footer students
