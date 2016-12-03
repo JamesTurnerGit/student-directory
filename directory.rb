bd879da71783af0c7a4858c2e7b4f54084802fcb
James Turner <mail.jt@gmail.com>
2016-12-03 23:41:43 +0000

    exercise 5

--- directory.rb
+++ directory.rb
@@ -1,22 +1,3 @@
-#array of students
-=begin
-students =[
-  {name: "Dr. Hannibal Lecter",         cohort: :november},
-  {name: "Darth Vader",                 cohort: :november},
-  {name: "Nurse Ratched",               cohort: :november},
-  {name: "Micheal Corleone",            cohort: :november},
-  {name: "Alex DeLarge",                cohort: :november},
-  {name: "The Wicked Witch of the West",cohort: :november},
-  {name: "T-2000",                      cohort: :november},
-  {name: "Freddy Krueger",              cohort: :november},
-  {name: "The Jocker",                  cohort: :november},
-  {name: "Joffrey Baratheon",           cohort: :november},
-  {name: "Norman  Bates",               cohort: :november}]
-=end
-
-#options for different exercises that mess with the output too much to be
-#sensible
-
 def input_students
   puts "please enter the names of the students if you want to enter a hobby"
   puts "put a comma then type the hobby"
@@ -48,12 +29,10 @@ def print_out names,filter
     puts "#{index}. #{name_text} #{cohort_text} #{hobby_text}"
     index += 1
   end
-  puts names
   return nil
 end
 
 def print_footer names
-  puts names
   puts "Overall, we have #{names.count} great students"
 end
 
@@ -67,7 +46,5 @@ students = input_students
 filter_character = get_filter
 
 print_header
-puts students
-print_out (students , filter_character)
-puts students
+print_out(students.clone ,filter_character)
 print_footer students
