530a6f08e7b8188481793be535b07dd331c6dc1a
James Turner <mail.jt@gmail.com>
2016-12-03 23:54:35 +0000

    refactoring attempt:

--- directory.rb
+++ directory.rb
@@ -1,11 +1,25 @@
+=begin
+In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort? What if one of the values is empty? Can you supply a default value? The input will be given to you as a string? How will you convert it to a symbol? What if the user makes a typo?
+
+Once you complete the previous exercise, change the way the users are displayed: print them grouped by cohorts. To do this, you'll need to get a list of all existing cohorts (the map() method may be useful but it's not the only option), iterate over it and only print the students from that cohort.
+
+Right now if we have only one student, the user will see a message "Now we have 1 students", whereas it should be "Now we have 1 student". How can you fix it so that it used singular form when appropriate and plural form otherwise?
+
+We've been using the chomp() method to get rid of the last return character. Find another method among those provided by the String class that could be used for the same purpose (although it will require passing some arguments).
+
+Once you have completed the "Asking for user input" section, open this file. It's Ruby code but it has some typos. Copy it to a local file and open it in Atom without syntax highlighting. To do this, select "Plain Text" in the lower right corner of the window. It should look similar to this: rubysyntaxhighlighting Now, find all typos in that file and correct them. Use your experience, online documentation, etc. to find all mistakes. Run the script in the terminal from time to time to make sure it works as it should. Google the errors Ruby will give you, think about what they could mean, try different things but don't look the answer up :)
+
+What happens if the user doesn't enter any students? It will try to print an empty list. How can you use an if statement (:pill: Control Flow) to only print the list if there is at least one student in there?
+=end
 def input_students
-  puts "please enter the names of the students if you want to enter a hobby"
-  puts "put a comma then type the hobby"
+  puts "please enter the names of the students. if you want to enter a hobby"
+  puts "put a comma then type the hobby, same for cohort"
   puts "To finish, just hit return"
   students = []
   input = gets.chomp.split(/,/)
   while !input.empty? do
-    students << {name: input[0],hobby: input[1], cohort: :november}
+    input[2] ||= Time.now.strftime("%B").downcase # default for cohort is this month
+    students << {name: input[0],hobby: input[1], cohort: input[2]}
     puts "Now we have #{students.count} students"
     input = gets.chomp
   end
@@ -23,10 +37,10 @@ def print_out names,filter
     student = names.shift
     next if filter && filter != student[:name][0]
     next if student[:name].length > 11
-    name_text  = "#{student[:name]}"
-    cohort_text  = "(#{student[:cohort]} cohort)"
+    name_text  = "#{student[:name]}".ljust 15
+    cohort_text  = "(#{student[:cohort]} cohort)".ljust 20
     hobby_text = student[:hobby] ? "hobbies :#{student[:hobby]}": ""
-    puts "#{(index.to_s + ".").ljust(3)} #{name_text.ljust(15)} #{cohort_text.ljust 20} #{hobby_text.ljust 20}"
+    puts "#{(index.to_s + ".").ljust(3)} #{name_text} #{cohort_text} #{hobby_text}"
     index += 1
   end
   return nil
